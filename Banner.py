ec47c2b66c02a8b170b70d6ff1fb8c04f4ae1243
mfk lihan34 <118593996+LOVER-BOY10@users.noreply.github.com>
2025-12-26 13:00:09 +0600

    Add files via upload

---
+++ Banner.py
@@ -0,0 +1,93 @@
+#import os
+#import sys
+#import time
+#os.system("clear")
+#print("\n\n\n")
+#Banner="			System Loading...."
+#for c in Banner:
+#	sys.stdout.write(c)
+#	sys.stdout.flush()
+#	time.sleep(0.2)
+
+#print("\n\n\n")
+#os.system("clear")
+#time.sleep(0.2)
+#print("\n\n\n")
+#Banner="			Successfully Loaded\n"
+#for c in Banner:
+#	sys.stdout.write(c)
+#	sys.stdout.flush()
+#	time.sleep(0.2)
+#Name=input("			Enter Your Name:")
+#Banner=("			Hay "+Name+",be Ethikal Hack")
+#for c in Banner:
+#	sys.stdout.write(c)
+#	sys.stdout.flush()
+#	time.sleep(0.2)
+#print("\n")
+#os.system("clear")
+#time.sleep(2)	
+#print("""
+#    __  __ ______ _  __  _      _____ _    _          _   _  
+# |  \\/  |  ____| |/ / | |    |_   _| |  | |   /\\   | \\ | | 
+# | \\  / | |__  | ' /  | |      | | | |__| |  /  \\  |  \\| | 
+# | |\\/| |  __| |  <   | |      | | |  __  | / /\\ \\ | . ` | 
+# | |  | | |    | . \\  | |____ _| |_| |  | |/ ____ \\| |\  | 
+# |_|  |_|_|    |_|\\_\\ |______|_____|_|  |_/_/    \\_\\_| \\_| 
+#===============================                                                           
+
+#\033[1;40mOwner : MFK LIHAN\033[0m
+#\033[1;36mGitHub : BANGLADESH\033[0m
+#\033[1;31mFacebook : FAZLUL KARIM\033[0m
+#\033[1;35mNumber : 01610052155\033[0m
+#\033[1;34m===========================\033[0m""")
+
+
+import os
+import sys
+import time
+os.system("clear")
+print("\n\n\n")
+mfk="		\033[1;32mSystem Loding...\033[0m"
+for c in mfk:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print("\n")
+
+
+import os
+import sys
+import time
+time.sleep(2)
+os.system("clear")
+print("\n\n\n")
+mfk="		\033[1;33mSuccessfully Lodad\n\033[0m"
+for c in mfk:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+Name=input("		\033[1;34mEnter Your Name:\033[0m")
+mfk=("		\033[1;35mHay \033[1;36m"+Name+",\033[1;38mBe Ethikal Hakc")
+for c in mfk:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print("\n")
+time.sleep(2)
+os.system("clear")
+print("""\033[1;40m
+ __  __ ______ _  __  _      _____ _    _          _   _ 
+ |  \\/  |  ____| |/ / | |    |_   _| |  | |   /\\   | \\ | |
+ | \\  / | |__  | ' /  | |      | | | |__| |  /  \\  |  \\| |
+ | |\\/| |  __| |  <   | |      | | |  __  | / /\\ \\ | . ` |
+ | |  | | |    | . \\  | |____ _| |_| |  | |/ ____ \\| |\\  |
+ |_|  |_|_|    |_|\\_\\ |______|_____|_|  |_/_/    \\_\\_| \\_|
+\033[1;30m====================================== ==\033[0m                                                       
+\033[1;40mOwner : MFK LIHAN\033[0m
+\033[1;36mGitHub : BANGLADESH\033[0m
+\033[1;31mFacebook : FAZLUL KARIM\033[0m
+\033[1;35mNumber : 01610052155\033[0m
+\033[1;34m========================================\033[0m""")
+
+	
